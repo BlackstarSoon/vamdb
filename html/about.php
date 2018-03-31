619edd123e4b6f3506525af30acfd8206dcaf18e
BlackstarSoon <weiliang317@outlook.com>
2018-03-31 16:11:38 +0800

    Add about us page

---
+++ html/about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About Us</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="shortcut icon" type="image/png" href="picture/flower.png" />
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="css/mystyle.css">
+
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<!--Header-->
+		<?php include ('../html/component/header.php'); ?>
+
+		<div class="container">
+			<h1>About Us</h1>
+
+			<p>What is this app for?</p>
+			<p>App app to help everyone to reduce stress with an aided deep breathing video (Flower Breathing and Bubble Breathing) training, adding the heart rate variability (HRV) for monitoring the changes before and after the training.</p>
+
+			<p>Why using video to guide deep breathing?</p>
+			<p>The videos show a flower with appearing of petals (breathing in) and disappearing of petals (breathing out); and also expanding (breathing in) and shrinking (breathing out) of buuble to aid or accompany one's to do the deep breathing. According to the research study, the suggested deep breathing for 6 full cycles in one minutes is recommended to avoid hyperventilation. Here, the videos with appropriate timing for deep breathing is guided. With a wander mind, guided object can help to let us stay focus on it and feeling relax in short duration.</p>
+
+			<p>What is Heart Rate Variability (HRV) and why do user is encouraged to do the measurement for before and after the deep breathing training?</p>
+			<p>Heart Rate Variability (HRV) is the physiological phenomenon of variation in the time interval between heartbeats. It is a non invasive tools in stress management with deep breathing. With this app, user needs not to purchase external HRV device to do the measurement. You can focus on two aspects.</p>
+			<ol>
+				<li>HRV measurement reliability and repeatability</li>
+				<li>HRV baseline values</li>
+			</ol>
+
+			<p>User may use the HRV baseline values (before training) to comapre with the result after deep breathing training. HRV baseline values is relevant in the context of longitudinal changes and comparisons. You need to have a higher HRV, which reflecting your heart achieving a more time constant heart beat rhythm. Anyhow, deeper breaths increase your HRV</p>
+		</div>
+		
+	</body>
+</html>
--- html/component/header.php
+++ html/component/header.php
@@ -26,13 +26,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">VAMDB</a>
+      <a class="navbar-brand" href="https://vamdbapp.herokuapp.com/index.php">VAMDB</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li class="active"><a href="https://vamdbapp.herokuapp.com/index.php">Home</a></li>
+        <li><a href="https://vamdbapp.herokuapp.com/html/about.php">About Us</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
