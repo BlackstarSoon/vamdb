a4f5ae263b010e0bb198e74e4faf60f4b7c6492d
BlackstarSoon <weiliang317@outlook.com>
2018-03-31 16:57:46 +0800

    Modified timezone

--- html/post.php
+++ html/post.php
@@ -1,5 +1,6 @@
 <!--php connection-->
 <?php
+	date_default_timezone_set('Asia/Kuching');
 	
 	$gender = [
 		'Male' => 'M',
@@ -18,22 +19,7 @@
 		die("Connection failed : ".$conn ->connect_error);
 	}
 
-	$stmt = $conn->prepare("INSERT INTO records (age, country, race, gender, weight, height, sdnn_before, sdnn_after, posted) VALUES (?,?,?,?,?,?,?,?,?)");
-	$stmt -> bind_param("isssiisss", $age, $country, $race, $gender, $weight, $height, $sdnn_before, $sdnn_after, $posted);
-
-	//set parameters
 	/*
-	$age = int($_POST['age']);
-	$country = 'Malaysia';
-	$race = 'Chinese';
-	$gender = 'M';
-	$weight = 83;
-	$height = 181;
-	$sdnn_before = '97.52';
-	$sdnn_after = '113.85';
-	$posted = date('Y-m-d H:i:s');
-	*/
-	
 	$age = intval($_POST["age"]);
 	$country = $_POST["country"];
 	$race = $_POST["race"];
@@ -43,10 +29,43 @@
 	$sdnn_before = $_POST["sdnn_before"];
 	$sdnn_after = $_POST["sdnn_after"];
 	$posted = date('Y-m-d H:i:s');
+	*/
 
+	$age = 20;
+	$country = 'Malaysia';
+	$race = 'Chinese';
+	$gender = 'M';
+	$weight = 83;
+	$height = 181;
+	$sdnn_before = '51.94';
+	$sdnn_after = '68.44';
+	$posted = date('Y-m-d H:i:s');
+
+	//prepare statement
+	$stmt = $conn ->prepare("SELECT * FROM records order by id desc limit 1"); 
 	
-	//execute
+	//execute statement
 	$stmt ->execute();
+
+	//get records
+	$records = mysqli_stmt_get_result($stmt);
+
+	var_dump($records);
+
+	if($records ->num_rows >0){
+		while($row = mysqli_fetch_array($records)){
+			if( $row["sdnn_before"] == $sdnn_before && $row["sdnn_after"] == $sdnn_after){
+				//do nothing
+				echo "Duplicate result";
+			}
+			else{
+				$stmt = $conn->prepare("INSERT INTO records (age, country, race, gender, weight, height, sdnn_before, sdnn_after, posted) VALUES (?,?,?,?,?,?,?,?,?)");
+				$stmt -> bind_param("isssiisss", $age, $country, $race, $gender, $weight, $height, $sdnn_before, $sdnn_after, $posted);
+				//execute
+				$stmt ->execute();
+			}
+		}
+	}
 
 	$stmt ->close();
 	$conn ->close();
